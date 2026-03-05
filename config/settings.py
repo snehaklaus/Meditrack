0713604137cdb54ec93be1125207fd3eb7806a01
sneha <sneha.naik1117@gmail.com>
2026-03-05 16:12:57 +0530

    SMTP via environment variables

--- config/settings.py
+++ config/settings.py
@@ -210,17 +210,14 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 USE_TZ = True
 
-# Email Configuration (for development, use console backend)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@meditrack.com'
-   
-   # For production, use:
-   # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-   # EMAIL_HOST = config('EMAIL_HOST')
-   # EMAIL_PORT = config('EMAIL_PORT', cast=int)
-   # EMAIL_USE_TLS = True
-   # EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-   # EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@meditrack.com')
+
 
 
 
