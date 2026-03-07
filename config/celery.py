ba0726bd17db6eb523cb3ebf4d0a5e5d8ec0b6d5
sneha <sneha.naik1117@gmail.com>
2026-03-07 12:32:07 +0530

    Shut down email servicefor the weekend

--- config/celery.py
+++ config/celery.py
@@ -9,10 +9,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'check-medication-reminders': {
-        'task': 'medications.tasks.send_medication_reminders',
-        'schedule': 900.0,  # every 10 minutes (change to crontab(minute=0) after testing)
-    },
+    #'check-medication-reminders': {
+     #   'task': 'medications.tasks.send_medication_reminders',
+      #  'schedule': 900.0,  # every 10 minutes (change to crontab(minute=0) after testing)
+    #},
     'send-weekly-digest-sunday-9am': {
         'task': 'medications.tasks.send_weekly_digest',
         'schedule': crontab(hour=9, minute=0, day_of_week=0),  # Sunday 09:00 UTC
